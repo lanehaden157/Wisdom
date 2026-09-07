90b588a00da2887ac051d293b4b26f66ade0958d
lanehaden157 <lane.haden157@gmail.com>
2026-09-07 11:50:19 -0500

    Wisdom — 2026-09-07

--- data/quote-edits.js
+++ data/quote-edits.js
@@ -8,6 +8,16 @@ window.Wisdom.quoteEdits = {
    "id": 100000,
    "text": "Where the resistance is shows me where I’m still not free",
    "category": "quote"
+  },
+  {
+   "id": 100001,
+   "text": "Where am I currently agnostic? Wherever there’s trouble in my life",
+   "category": "quote"
+  },
+  {
+   "id": 100002,
+   "text": "We agnostics could be renamed, “How’s it workin’ out for ya?”",
+   "category": "quote"
   }
  ]
 };
