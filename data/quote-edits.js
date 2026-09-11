8c95bdb68e339a83d193db895d6f55aea9d5c5d5
lanehaden157 <lane.haden157@gmail.com>
2026-09-11 09:40:47 -0500

    Wisdom — 2026-09-11

--- data/quote-edits.js
+++ data/quote-edits.js
@@ -58,6 +58,11 @@ window.Wisdom.quoteEdits = {
    "id": 945,
    "text": "The Man In The Glass\nWhen you get what you want in your struggle for selt, And the world makes you King for a day, Then go to the mirror and look at yourself, And see what that man has to say.\nFor it isn't your Father, or Mother, or Wife, who judgment upon you must pass.\nThe fellow whose verdict counts most in your life\nIs the man staring back from the glass.\nHe's the fellow to please, never mind all the rest, For he's with you clear up to the end\nAnd you've passed your most dangerous, difficult test\nIf the man in the glass is your friend.\nYou may be like Jack Horner and \"chisel\" a plum, And think you're a wonderful guy,\nBut the guy in the glass says you're only a bum\nIf you can't look him straight in the eye.\nYou can fool the whole world down the pathway of years, And get pats on the back as you pass,\nBut your final reward will be heartaches and tears\nIf you've cheated the man in the glass.",
    "category": "poem"
+  },
+  {
+   "id": 946,
+   "text": "You can know the law, but I’d rather know the judge",
+   "category": "quote"
   }
  ]
 };
